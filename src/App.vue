e398f0b87466b134f3433608212a73845b143868
annieremeeva <annieremeeva@gmail.com>
2024-07-24 20:57:45 +0300

    placed icons in other file

--- src/App.vue
+++ src/App.vue
@@ -7,72 +7,11 @@ import WeatherMain from "./components/WeatherMain.vue";
 import { Icon } from "@iconify/vue";
 import { useGeolocation } from "@vueuse/core";
 import { watch, onMounted, ref, computed } from "vue";
+import { weatherIcons } from "./icons";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
-const weatherIcons = {
-  a01d: "material-symbols:mist",
-  a01n: "material-symbols:mist",
-  a02d: "fluent:weather-fog-20-regular",
-  a02n: "fluent:weather-fog-20-regular",
-  a03d: "fluent:weather-fog-20-regular",
-  a03n: "fluent:weather-fog-20-regular",
-  a04d: "fluent:weather-fog-20-regular",
-  a04n: "fluent:weather-fog-20-regular",
-  a05d: "fluent:weather-fog-20-regular",
-  a05n: "fluent:weather-fog-20-regular",
-  a06d: "fluent:weather-fog-20-regular",
-  a06n: "fluent:weather-fog-20-regular",
-  c01d: "fluent:weather-sunny-16-regular",
-  c01n: "fluent:weather-moon-16-regular",
-  c02d: "fluent:weather-partly-cloudy-day-16-regular",
-  c02n: "fluent:weather-partly-cloudy-night-24-regular",
-  c03d: "fluent:weather-partly-cloudy-day-16-regular",
-  c04d: "fluent:weather-cloudy-24-regular",
-  c04n: "fluent:weather-cloudy-24-regular",
-  d01d: "fluent:weather-drizzle-20-regular",
-  d01n: "fluent:weather-drizzle-20-regular",
-  d02d: "fluent:weather-drizzle-20-regular",
-  d02n: "fluent:weather-drizzle-20-regular",
-  d03d: "fluent:weather-drizzle-20-regular",
-  d03n: "fluent:weather-drizzle-20-regular",
-  f01d: "fluent:weather-rain-snow-20-regular",
-  f01n: "fluent:weather-rain-snow-20-regular",
-  r01d: "fluent:weather-rain-20-regular",
-  r01n: "fluent:weather-rain-20-regular",
-  r02d: "fluent:weather-rain-20-regular",
-  r02n: "fluent:weather-rain-20-regular",
-  r03d: "fluent:weather-rain-20-regular",
-  r03n: "fluent:weather-rain-20-regular",
-  r04d: "fluent:weather-rain-showers-day-20-regular",
-  r04n: "fluent:weather-rain-shower-night-20-regular",
-  r05d: "fluent:weather-rain-showers-day-20-regular",
-  r05n: "fluent:weather-rain-shower-night-20-regular",
-  r06d: "fluent:weather-rain-showers-day-20-regular",
-  r06n: "fluent:weather-rain-shower-night-20-regular",
-  s01d: "fluent:weather-snow-shower-day-20-regular",
-  s01n: "fluent:weather-snow-shower-night-20-regular",
-  s02d: "fluent:weather-snow-20-regular",
-  s02n: "fluent:weather-snow-20-regular",
-  s03d: "fluent:weather-snow-20-regular",
-  s03n: "fluent:weather-snow-20-regular",
-  s04d: "fluent:weather-rain-snow-20-regular",
-  s04n: "fluent:weather-rain-snow-20-regular",
-  s05d: "fluent:weather-blowing-snow-20-regular",
-  s05n: "fluent:weather-blowing-snow-20-regular",
-  s06d: "fluent:weather-snow-20-regular",
-  s06n: "fluent:weather-snow-20-regular",
-  t01d: "fluent:weather-thunderstorm-20-regular",
-  t01n: "fluent:weather-thunderstorm-20-regular",
-  t02d: "fluent:weather-thunderstorm-20-regular",
-  t02n: "fluent:weather-thunderstorm-20-regular",
-  t03d: "fluent:weather-thunderstorm-20-regular",
-  t03n: "fluent:weather-thunderstorm-20-regular",
-  t04d: "fluent:weather-thunderstorm-20-regular",
-  t04n: "fluent:weather-thunderstorm-20-regular",
-  t05d: "fluent:weather-thunderstorm-20-regular",
-  t05n: "fluent:weather-thunderstorm-20-regular",
-};
+weatherIcons.a01n = weatherIcons.a01d;
 
 const { coords, locatedAt, error, resume, pause } = useGeolocation();
 let weatherData = ref({});
@@ -169,6 +108,7 @@ async function fetchWeatherData() {
       },
     });
     weatherData.value = JSON.parse(JSON.stringify(response.data.data[0]));
+    console.log(weatherData.value);
   } catch (e) {
     console.log(e.message);
   }
--- src/components/TodayHighlights.vue
+++ src/components/TodayHighlights.vue
@@ -2,6 +2,33 @@
 import WeatherCard from "./UI/WeatherCard.vue";
 import WeatherInnerCard from "./UI/WeatherInnerCard.vue";
 import { Icon } from "@iconify/vue";
+
+const props = defineProps({
+  windSpeed: {
+    type: Number,
+  },
+  windDir: {
+    type: String,
+  },
+  windGust: {
+    type: Number,
+  },
+  uv: {
+    type: Number,
+  },
+  appTemp: {
+    type: Number,
+  },
+  pressure: {
+    type: Number,
+  },
+  sunrise: {
+    type: String,
+  },
+  sunset: {
+    type: String,
+  },
+});
 </script>
 
 <template>
@@ -9,25 +36,73 @@ import { Icon } from "@iconify/vue";
     <div class="highlights-column">
       <WeatherInnerCard class="highlights-card">
         <h2>Ветер</h2>
+        <Icon
+          class="icon"
+          icon="ph:waveform"
+          width="64"
+          height="64"
+          style="color: white"
+        />
+        <p>{{}}</p>
       </WeatherInnerCard>
       <WeatherInnerCard class="highlights-card-small">
         <h2>Порывы ветра</h2>
+        <Icon
+          class="icon"
+          icon="fluent:weather-squalls-20-filled"
+          width="64"
+          height="64"
+          style="color: white"
+        />
+        <p>{{}}</p>
       </WeatherInnerCard>
     </div>
     <div class="highlights-column">
       <WeatherInnerCard class="highlights-card">
         <h2>Давление</h2>
+        <Icon
+          class="icon"
+          icon="lets-icons:pressure"
+          width="64"
+          height="64"
+          style="color: white"
+        />
+        <p>{{}}</p>
       </WeatherInnerCard>
       <WeatherInnerCard class="highlights-card-small">
         <h2>Ощущается</h2>
+        <Icon
+          class="icon"
+          icon="carbon:temperature-feels-like"
+          width="64"
+          height="64"
+          style="color: white"
+        />
+        <p>{{}}</p>
       </WeatherInnerCard>
     </div>
     <div class="highlights-column">
       <WeatherInnerCard class="highlights-card">
         <h2>Рассвет и закат</h2>
+        <Icon
+          class="icon"
+          icon="uil:sunset"
+          width="64"
+          height="64"
+          style="color: white"
+        />
+        <p>{{}}</p>
       </WeatherInnerCard>
       <WeatherInnerCard class="highlights-card-small">
         <h2>UV-индекс</h2>
+        <Icon
+          class="icon"
+          icon="hugeicons:uv-02"
+          width="64"
+          height="64"
+          style="color: white"
+        />
+        <p>{{}}</p>
       </WeatherInnerCard>
     </div>
   </WeatherCard>
@@ -42,7 +117,6 @@ import { Icon } from "@iconify/vue";
 }
 
 .highlights-column {
-  width: 33%;
   height: 100%;
   gap: 20px;
   display: flex;
@@ -50,12 +124,26 @@ import { Icon } from "@iconify/vue";
 }
 
 .highlights-card {
-  height: 60%;
+  height: 300px;
+  gap: 30px;
+}
+
+.highlights-card-small {
+  height: 200px;
+  gap: 20px;
+}
+
+.icon {
+  align-self: center;
 }
 
 @media (orientation: landscape) {
   .highlights {
     flex-direction: row;
+  }
+
+  .highlights-column {
+    width: 33%;
   }
 }
 </style>
